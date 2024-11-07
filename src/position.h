122666d386f0ade0c34883363a083fe258bd0191
9199126091_1421 <135975396+AriaKafie@users.noreply.github.com>
2024-11-06 17:43:54 -0800

    Add files via upload

---
+++ src/position.h
@@ -0,0 +1,69 @@
+
+#ifndef POSITION_H
+#define POSITION_H
+
+#include <string>
+#include <vector>
+
+#include "bitboard.h"
+#include "types.h"
+
+constexpr int MAX_MOVES = 128;
+
+enum GameState { ONGOING, MATE, STALEMATE, REPETITION, FIFTY_MOVE };
+
+struct StateInfo
+{
+    Square  ep_sq;
+    uint8_t castling_rights;
+    Color   side_to_move;
+    uint8_t halfmove_clock;
+};
+
+class Position
+{
+public:
+    Position() { set(); }
+
+    static void init();
+
+    Move *get_moves(Move *list);
+
+    void set(const std::string& fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 ");
+    void do_move(Move m);
+    std::string fen() const;
+    std::string to_string() const;
+    GameState game_state();
+    uint64_t hash() const;
+
+    bool kingside_rights  (Color Perspective) const { return state_info.castling_rights & (Perspective == WHITE ? 0b1000 : 0b0010); }
+    bool queenside_rights (Color Perspective) const { return state_info.castling_rights & (Perspective == WHITE ? 0b0100 : 0b0001); }
+
+    bool white_to_move() const { return state_info.side_to_move == WHITE; }
+    bool black_to_move() const { return state_info.side_to_move == BLACK; }
+
+    Color side_to_move() const { return state_info.side_to_move; }
+
+    Piece piece_on(Square sq) const { return board[sq]; }
+
+    PieceType piece_type_on(Square sq) const { return type_of(board[sq]); }
+
+    Bitboard bb(Piece pc) const { return bitboards[pc]; }
+
+    Bitboard occupied() const { return bitboards[WHITE] | bitboards[BLACK]; }
+
+    Bitboard ep_bb() const { return square_bb(state_info.ep_sq); }
+
+    Square ep_sq() const { return state_info.ep_sq; }
+    
+private:
+    void update_castling_rights(Color just_moved);
+
+    Bitboard bitboards[16];
+    Piece board[SQUARE_NB];
+
+    StateInfo state_info;
+    std::vector<uint64_t> history;
+};
+
+#endif
