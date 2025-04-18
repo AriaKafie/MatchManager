419ab8a791875215d4eeb23441ace11c4593b070
9199126091_1421 <135975396+AriaKafie@users.noreply.github.com>
2025-04-18 11:03:33 -0700

    Add files via upload

--- src/mm.cpp
+++ src/mm.cpp
@@ -16,6 +16,29 @@
 #include "engine.h"
 #include "position.h"
 
+template<typename T>
+bool get_opt(const std::string& option, T& var, int argc, char *argv[])
+{
+    std::string opt = "-" + option, args, token;
+
+    for (int i = 3; i < argc; i++)
+        args += std::string(argv[i]) + " ";
+
+    for (std::istringstream is(args); is >> token;)
+    {
+        if (token == opt)
+            return bool(is >> var);
+
+        if (token.find(opt) == 0)
+        {
+            std::istringstream i(token.substr(opt.length()));
+            return bool(i >> var);
+        }
+    }
+
+    return true;
+}
+
 std::string time_()
 {
     std::time_t now_c = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
@@ -29,7 +52,7 @@ std::string time_()
     return ss.str();
 }
 
-void process_stdin(Status *status)
+void handle_stdin(Status *status)
 {
     std::string in;
 
@@ -135,6 +158,8 @@ void Match::run_games(Status *status)
 
 int main(int argc, char *argv[])
 {
+    std::string usage = "Usage: MatchManager <engine1> <engine2> [-threads <threads>] [-time <milliseconds>] [-fen <fenfile>]";
+
     Bitboards::init();
     Position::init();
 
@@ -151,24 +176,16 @@ int main(int argc, char *argv[])
 
     if (!(args >> name_1 >> name_2))
     {
-        std::cout << "Usage: MatchManager <engine1> <engine2> [options...]" << std::endl;
+        std::cout << usage << std::endl;
         return 1;
     }
 
-    while (args >> token)
+    if (!( get_opt("time", time, argc, argv)
+        && get_opt("threads", threads, argc, argv)
+        && get_opt("fen", fenpath, argc, argv)))
     {
-        if (token.find("--time=") == 0)
-        {
-            time = std::stoi(token.substr(token.find('=') + 1));
-        }
-        else if (token.find("--threads=") == 0)
-        {
-            threads = std::stoi(token.substr(token.find('=') + 1));
-        }
-        else if (token.find("--fen=") == 0)
-        {
-            fenpath = token.substr(token.find('=') + 1);
-        }
+        std::cout << usage << std::endl;
+        return 1;
     }
 
     std::string path_1 = std::string("engines\\") + name_1 + ".exe";
@@ -178,7 +195,7 @@ int main(int argc, char *argv[])
     std::vector<std::thread> thread_pool;
 
     Status status = GO;
-    std::thread t(process_stdin, &status);
+    std::thread t(handle_stdin, &status);
     t.detach();
 
     for (int id = 0; id < threads; id++)
@@ -190,7 +207,7 @@ int main(int argc, char *argv[])
     for (std::thread& thread : thread_pool)
         thread.join();
 
-    double e1_wins = 0, e2_wins = 0, draws = 0;
+    int e1_wins = 0, e2_wins = 0, draws = 0;
 
     for (Match *m : matches)
     {
@@ -201,19 +218,19 @@ int main(int argc, char *argv[])
         delete m;
     }
 
-    double total = e1_wins + e2_wins + draws, decisive = total - draws;
+    int total = e1_wins + e2_wins + draws, decisive = total - draws;
     
-    double e1_winrate = decisive > 0 ? double(e1_wins) / decisive : 0.0;
-    double e2_winrate = decisive > 0 ? double(e2_wins) / decisive : 0.0;
+    double e1_winrate = decisive > 0 ? e1_wins / decisive : 0.0;
+    double e2_winrate = decisive > 0 ? e2_wins / decisive : 0.0;
 
     printf("+-----------------+-------+----------+\n");
     printf("|     Outcome     |   #   | Win Rate |\n");
     printf("+-----------------+-------+----------+\n");
 
-    printf("| %-16s|%6d |%8.2f%% |\n", name_1.c_str(), (int)e1_wins, e1_winrate * 100);
-    printf("| %-16s|%6d |%8.2f%% |\n", name_2.c_str(), (int)e2_wins, e2_winrate * 100);
-    printf("| Draws           |%6d |          |\n", (int)draws);
-    printf("| Total           |%6d |%6d ms |\n+-----------------+-------+----------+\n", (int)total, time);
+    printf("| %-16s|%6d |%8.2f%% |\n", name_1.c_str(), e1_wins, e1_winrate * 100);
+    printf("| %-16s|%6d |%8.2f%% |\n", name_2.c_str(), e2_wins, e2_winrate * 100);
+    printf("| Draws           |%6d |          |\n", draws);
+    printf("| Total           |%6d |%6d ms |\n+-----------------+-------+----------+\n", total, time);
 
     printf("%s is %f (+/- %f) elo away from %s\n",
         name_1.c_str(), elo_diff(e1_wins, e2_wins, draws), elo_margin(e1_wins, e2_wins, draws), name_2.c_str());
